123563fe3815aade92cb53ab55edc277e4b57aff
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2023-10-23 15:21:34 +0300

    NameSpaceFixes

--- Task8.Data/Data/Task6Context.cs
+++ Task8.Data/Data/Task6Context.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using Microsoft.EntityFrameworkCore;
-using Task8.Data.Entitys.Generated;
+using Task8.Data.Entity.Generated;
 
 namespace Task8.Data.Data;
 
