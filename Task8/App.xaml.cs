c5be689a0194b62a69682d9671dedc9d29d38674
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2023-10-23 13:09:18 +0300

    Добавьте файлы проекта.

---
+++ Task8/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using System.Windows;
+using Task8.Views;
+
+namespace Task8
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
