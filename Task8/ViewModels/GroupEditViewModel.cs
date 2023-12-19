c7bf2eff00d41c02a6a686329106c56bd3444850
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2023-12-19 15:05:29 +0200

    Add dependencies to GroupEditModel

--- Task8/ViewModels/GroupEditViewModel.cs
+++ Task8/ViewModels/GroupEditViewModel.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using Prism.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task8.BL.Interfaces;
 
 namespace Task8.ViewModels
 {
     public class GroupEditViewModel
     {
+        private readonly IGroupEditModel _model;
 
+        public GroupEditViewModel(IGroupEditModel model, IEventAggregator eventAggregator)
+        {
+            _model = model;
+        }
     }
 }
