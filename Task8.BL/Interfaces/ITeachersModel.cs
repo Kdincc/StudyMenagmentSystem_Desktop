29d05417ad4df3d678084324df153922a3d1bbb5
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2023-12-20 13:00:40 +0200

    Add ITeacherModel interface

---
+++ Task8.BL/Interfaces/ITeachersModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task8.Data.Entity.Generated;
+
+namespace Task8.BL.Interfaces
+{
+    public interface ITeachersModel
+    {
+        public IEnumerable<Teacher> Teachers{ get; }
+
+        public void SaveChangesFor(Teacher teacher);
+
+        public void Remove(Teacher teacher);
+    }
+}
