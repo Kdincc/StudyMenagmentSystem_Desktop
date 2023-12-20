42edff5d24a5f5c45d0d375ca24c0d005e970636
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2023-12-20 13:01:28 +0200

    Add TeachersModel

---
+++ Task8.BL/Models/TeachersModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task8.BL.Interfaces;
+using Task8.Data.Entity.Generated;
+
+namespace Task8.BL.Models
+{
+    public class TeachersModel : ITeachersModel
+    {
+        public IEnumerable<Teacher> Teachers => throw new NotImplementedException();
+
+        public void Remove(Teacher teacher)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChangesFor(Teacher teacher)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
