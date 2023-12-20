971cf4d2dc837843edcae44600693950203bd3d5
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2023-12-20 13:06:40 +0200

    Add dependecies in TeachersViewModel

--- Task8/ViewModels/TeachersViewModel.cs
+++ Task8/ViewModels/TeachersViewModel.cs
@@ -4,10 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task8.BL.Interfaces;
 
 namespace Task8.ViewModels
 {
     public class TeachersViewModel : BindableBase
     {
+        private readonly ITeachersModel _teachersModel;
+
+        public TeachersViewModel(ITeachersModel teachersModel)
+        {
+            _teachersModel = teachersModel;
+        }
     }
 }
