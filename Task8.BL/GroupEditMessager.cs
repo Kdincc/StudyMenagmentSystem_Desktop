faba66ca196c68903ea0ae8df42fa9431839b153
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2023-12-21 17:04:50 +0200

    Add GroupEditMessager

---
+++ Task8.BL/GroupEditMessager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Task8.BL
+{
+    public static class GroupEditMessager
+    {
+        public static void EmptyStudentNameMessage() 
+        {
+            MessageBox.Show("Name and surname of the student can`t be empty !", "", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
