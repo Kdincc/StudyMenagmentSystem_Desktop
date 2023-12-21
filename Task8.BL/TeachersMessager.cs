4876cc96586263d1f3149473aa55779207105c1f
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2023-12-21 17:07:10 +0200

    Add TeacherMessager

---
+++ Task8.BL/TeachersMessager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Task8.BL
+{
+    public static class TeachersMessager
+    {
+        public static void EmptyTeacherNameMessage()
+        {
+            MessageBox.Show("Name and surname of the teacher can`t be empty !", "", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
