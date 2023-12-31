66bb2e028183a38dbb2fc7babf904f7960b8cb54
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2023-12-31 17:04:20 +0200

    Implement BuildGroupReport method

--- Task8.BL/ReportBuilder.cs
+++ Task8.BL/ReportBuilder.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task8.Data.Entity.Generated;
 
 namespace Task8.BL
 {
-    internal class ReportBuilder
+    public static class ReportBuilder
     {
+        public static GroupReport BuildGroupReport(Group group) 
+        {
+            return new GroupReport(group.Course.Name, group.Name, group.Students.ToList());
+        }
     }
 }
