2a8cfa158bb60b587a21674fa41c51062364708f
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2023-12-31 16:58:08 +0200

    Add IReportBuilder

---
+++ Task8.BL/Interfaces/ReportBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task8.Data.Entity.Generated;
+
+namespace Task8.BL.Interfaces
+{
+    public interface IReportBuilder
+    {
+        public GroupReport BuildGroupReport(Group group);
+    }
+}
