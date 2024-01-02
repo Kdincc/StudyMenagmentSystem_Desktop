9bc76c6ef8c525fb608f3f5efb00fed6a3ee608f
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-01-02 18:01:50 +0200

    Add RepositoryServiceTests

---
+++ Task8.Tests/RepositoryServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task8.BL.Interfaces;
+using Task8.Data.Data;
+
+namespace Task8.Tests
+{
+    [TestClass]
+    public class RepositoryServiceTests
+    {
+    }
+}
