cc5977b5e05a2dfe7cbcfd892f98c8c3af53e730
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-01-03 14:17:52 +0200

    Remove unnessessary

--- Task8.Data/Entity/Teacher.cs
+++ Task8.Data/Entity/Teacher.cs
@@ -5,17 +5,5 @@ namespace Task8.Data.Entity.Generated
     public partial class Teacher
     {
         public string FullName => $"{Name} {Surname}";
-
-        public Teacher(Teacher teacher)
-        {
-            Name = teacher.Name;
-            Surname = teacher.Surname;
-            TeacherId = teacher.TeacherId;
-            Groups = teacher.Groups.ToList();
-        }
-
-        public Teacher() 
-        {
-        }
     }
 }
